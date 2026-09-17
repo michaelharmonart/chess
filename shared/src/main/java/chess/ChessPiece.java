d521e9ca69722e7abdcbc12c19b7b46eabc60154
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-09-17 17:57:11 -0600

    pawn moveset and refactor

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -55,24 +55,102 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
-        if (piece.getPieceType() == PieceType.BISHOP) {
-            return getDirectionalMoves(board, myPosition, new int[][]{{1, -1}, {1, 1}, {-1, -1}, {-1, 1}});
-        }
-        if (piece.getPieceType() == PieceType.KING) {
-            return getStepMoves(board, myPosition, new int[][]{{1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}});
-        }
-        if (piece.getPieceType() == PieceType.KNIGHT) {
-            return getStepMoves(board, myPosition, new int[][]{{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}});
-        }
-        if (piece.getPieceType() == PieceType.QUEEN) {
-            return getDirectionalMoves(board, myPosition, new int[][]{{1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}});
-        }
-        if (piece.getPieceType() == PieceType.ROOK) {
-            return getDirectionalMoves(board, myPosition, new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}});
-        }
-
-        return List.of();
+        return switch (piece.getPieceType()) {
+            case PieceType.BISHOP -> bishopMoves(board, myPosition);
+            case PieceType.KING -> kingMoves(board, myPosition);
+            case PieceType.KNIGHT -> knightMoves(board, myPosition);
+            case PieceType.QUEEN -> queenMoves(board, myPosition);
+            case PieceType.ROOK -> rookMoves(board, myPosition);
+            case PieceType.PAWN -> pawnMoves(board, myPosition);
+        };
     }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition initialPosition) {
+        return getDirectionalMoves(board, initialPosition, new int[][]{{1, -1}, {1, 1}, {-1, -1}, {-1, 1}});
+    }
+
+    private Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition initialPosition) {
+        return getStepMoves(board, initialPosition, new int[][]{{1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}});
+    }
+
+    private Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition initialPosition) {
+        return getStepMoves(board, initialPosition, new int[][]{{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}});
+    }
+
+    private Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition initialPosition) {
+        return getDirectionalMoves(board, initialPosition, new int[][]{{1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}});
+    }
+
+    private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition initialPosition) {
+        return getDirectionalMoves(board, initialPosition, new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}});
+    }
+
+    private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition initialPosition) {
+        List<ChessMove> validMoves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(initialPosition);
+        ChessGame.TeamColor teamColor = piece.getTeamColor();
+
+        int[][] moveOffsets = switch (teamColor) {
+            case ChessGame.TeamColor.WHITE -> new int[][]{{1, 0}, {1, -1}, {1, 1}};
+            case ChessGame.TeamColor.BLACK -> new int[][]{{-1, 0}, {-1, -1}, {-1, 1}};
+        };
+
+        int initialRow = switch (teamColor) {
+            case ChessGame.TeamColor.WHITE -> 2;
+            case ChessGame.TeamColor.BLACK -> 7;
+        };
+
+        int[] doubleMoveOffset = switch (teamColor) {
+            case ChessGame.TeamColor.WHITE -> new int[]{2, 0};
+            case ChessGame.TeamColor.BLACK -> new int[]{-2, 0};
+        };
+
+
+        int promotionRow = switch (teamColor) {
+            case ChessGame.TeamColor.WHITE -> 8;
+            case ChessGame.TeamColor.BLACK -> 1;
+        };
+
+        for (int[] moveOffset : moveOffsets) {
+            ChessPosition targetPosition = new ChessPosition(initialPosition.getRow() + moveOffset[0], initialPosition.getColumn() + moveOffset[1]);
+            if (!board.isValidSquare(targetPosition)) {
+                continue;
+            }
+            ChessPiece targetPiece = board.getPiece(targetPosition);
+            if (targetPiece == null) {
+                if (moveOffset[1] != 0) {
+                    continue;
+                }
+            } else {
+                if (targetPiece.getTeamColor() == teamColor) {
+                    continue;
+                }
+                if (moveOffset[1] == 0) {
+                    continue;
+                }
+            }
+
+            if (targetPosition.getRow() == promotionRow) {
+                for (PieceType type : new PieceType[]{PieceType.QUEEN, PieceType.ROOK, PieceType.BISHOP, PieceType.KNIGHT}) {
+                    validMoves.add(new ChessMove(initialPosition, targetPosition, type));
+                }
+            } else {
+                validMoves.add(new ChessMove(initialPosition, targetPosition, null));
+                if (moveOffset[1] == 0) {
+                    if (initialPosition.getRow() == initialRow) {
+                        targetPosition = new ChessPosition(initialPosition.getRow() + doubleMoveOffset[0], initialPosition.getColumn() + doubleMoveOffset[1]);
+                        targetPiece = board.getPiece(targetPosition);
+                        if (targetPiece == null) {
+                            validMoves.add(new ChessMove(initialPosition, targetPosition, null));
+                        }
+                    }
+                }
+
+            }
+        }
+        return validMoves;
+    }
+
 
     private Collection<ChessMove> getDirectionalMoves(ChessBoard board, ChessPosition initialPosition, int[][] directions) {
         List<ChessMove> validMoves = new ArrayList<>();
